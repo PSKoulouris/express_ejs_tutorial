18bfd773729860df6ae5f7294cb8f24259409154
PSKoulouris <philippe.koulouris@gmail.com>
2025-09-22 09:47:45 +0200

    error 404 handling-optimization

--- routes/default.js
+++ routes/default.js
@@ -27,3 +27,20 @@ router.get("/about", function(req,res){
 module.exports = router
 
 */
+
+const express = require('express')
+
+const router = express.Router()
+
+router.get('/',function(req,res){
+    /*const htmlFilePath = path.join(__dirname,'views','index.html')
+    res.sendFile(htmlFilePath)*/
+
+    res.render('index')
+})
+
+router.get('/about',function(req,res){
+    res.render('about')
+})
+
+module.exports = router
--- routes/restaurants.js
+++ routes/restaurants.js
@@ -1,18 +1,67 @@
-/*
-
 //add all functions required for the restaurants:
 
-const express = express()
+const express = require('express')
 
 const router = express.Router()
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+router.get('/restaurants',function(req,res){
+    const filePath = path.join(__dirname,'data','restaurants.json')
+
+    const fileData =  fs.readFileSync(filePath)
+    const restaurants = JSON.parse(fileData)
+
+    res.render('restaurants', {numberOfRestaurants : restaurants.length, storedRests :restaurants})
+})
+
+router.get('/restaurants/:rid',function(req,res){
+    // To send back the right restaurant details!
+    const restaurantId = req.params.rid
+    //console.log(restaurantId)
+
+    const filePath = path.join(__dirname,'data','restaurants.json')
+    const fileData = fs.readFileSync(filePath)
+
+    const restaurants = JSON.parse(fileData)
+
+    for(const restaurant of restaurants){
+        if(restaurantId === restaurant.rId){
+           return res.render('restaurant-details',{restaurant}) 
+        }
+    }
+})
+
+router.get('/recommend',function(req,res){
+    res.render('recommend')
+})
 
 
+router.post('/recommend', function(req,res){
+    const restaurant = req.body
+    restaurant.rId = uuid.v4()
 
+    const filePath = path.join(__dirname, 'data','restaurants.json')
+    
+    const fileData = fs.readFileSync(filePath)
+    const restaurants = JSON.parse(fileData)
+    //restaurants.rId = restaurants.length
+    restaurants.push(restaurant)
 
+    fs.writeFileSync(filePath, JSON.stringify(restaurants))
 
+    res.redirect('confirm')
+})
 
+router.get('/confirm',function(req,res){
+    res.render('confirm')
+})
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 module.exports = router
 
-*/
